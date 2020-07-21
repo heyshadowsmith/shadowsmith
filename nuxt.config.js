f8e6c93f5e4893b511f70263d846b71079dfc61c
Shadow Smith <shadow@businesscreditreports.com>
2020-07-21 15:17:40 -0500

    Debug deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,11 @@ export default {
   */
   mode: 'universal',
   /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'static',
+  /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
   */
