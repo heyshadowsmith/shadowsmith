0dc70ac7ec791d92ef77d16924a8131d93d6d7ac
Shadow Smith <hey@shadowsmith.com>
2020-07-21 21:26:11 -0500

    Fix y axis padding for article link rows

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtLink v-for="(article, index) in articles" :key="index" :to="`/${article.slug}`" class="flex flex-col-reverse items-start sm:flex-row sm:items-end py-4 border-t border-b border-transparent hover:border-gray-200">
+    <NuxtLink v-for="(article, index) in articles" :key="index" :to="`/${article.slug}`" class="flex flex-col-reverse items-start py-2 sm:flex-row sm:items-end sm:py-3 border-t border-b border-transparent hover:border-gray-200">
       <span class="font-mono text-gray-500 text-sm mr-6">{{ formatDate(article.createdAt) }} </span><span class="font-medium text-gray-900">
         {{ article.title }}
       </span>
