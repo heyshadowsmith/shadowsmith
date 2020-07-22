f85640a9a3a46373a1b96183ae265f69c89f581f
Shadow Smith <hey@shadowsmith.com>
2020-07-21 21:25:20 -0500

    Add Articles link to Navbar

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -3,9 +3,14 @@
     <nuxt-link to="/">
       <Logo />
     </nuxt-link>
-    <nuxt-link to="/about" class="font-medium">
-      About
-    </nuxt-link>
+    <div>
+      <nuxt-link to="/" class="font-medium py-3 px-6">
+        Articles
+      </nuxt-link>
+      <nuxt-link to="/about" class="font-medium py-3 pl-6">
+        About
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
