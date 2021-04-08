1531dfd066a56e569eb165ddbaa5fe7c5cc5fd75
Shadow Smith <hey@shadowsmith.com>
2021-04-08 00:22:45 -0500

    Add Footer component

---
+++ components/layout/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="py-12 text-xs text-gray-500">
+    <p class="mb-1">
+      This website was proudly made with <a class="underline" target="_blank" href="https://nuxtjs.org">Nuxt</a>, styled with <a class="underline" target="_blank" href="https://tailwindcss.com">TailwindCSS</a>, and deployed to <a class="underline" target="_blank" href="https://vercel.com">Vercel</a>.
+    </p>
+    <p>
+      If you're interested in reaching out, my Twitter handle is <a class="underline" target="_blank" href="https://twitter.com/heyshadowsmith">@heyshadowsmith</a>.
+    </p>
+  </div>
+</template>
