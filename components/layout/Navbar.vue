5a3fa87ebe0e5f7110f0508b4c05b38c2f92bf19
Shadow Smith <hey@shadowsmith.com>
2021-04-08 22:02:14 -0500

    Refactor Navbar

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="flex flex-col justify-between items-center sm:flex-row py-6">
-    <nuxt-link to="/" class="mb-6 sm:mb-0">
+  <div class="flex justify-between items-center py-6 mb-6 sm:mb-0">
+    <nuxt-link to="/">
       <Logo />
     </nuxt-link>
-    <div>
-      <nuxt-link to="/" class="font-medium py-3 px-6">
-        Articles
-      </nuxt-link>
-      <nuxt-link to="/music" class="font-medium py-3 px-6">
-        Music
-      </nuxt-link>
-      <nuxt-link to="/about" class="font-medium py-3 px-6 sm:pr-0">
-        About
-      </nuxt-link>
-    </div>
+    <nuxt-link to="/menu" class="font-medium py-3 px-6">
+      Menu
+    </nuxt-link>
   </div>
 </template>
