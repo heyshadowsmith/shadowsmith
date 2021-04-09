435a1becdd01260ab0000cde2c7c5fab53f5ee34
Shadow Smith <hey@shadowsmith.com>
2021-04-08 22:02:59 -0500

    Add menu page

---
+++ pages/menu.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <NuxtLink v-for="(link, index) in links" :key="index" :to="link.path" class="menu-link flex items-start py-2 sm:flex-row sm:items-end sm:py-3 border-t border-b border-transparent hover:border-gray-200">
+      <span class="font-medium text-gray-900">
+        {{ link.title }}
+      </span>
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      links: [
+        {
+          title: 'Articles',
+          path: '/'
+        },
+        {
+          title: 'Music',
+          path: '/music'
+        },
+        {
+          title: 'About',
+          path: '/about'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.menu-link {
+  transition: all 0.25s ease;
+}
+</style>
